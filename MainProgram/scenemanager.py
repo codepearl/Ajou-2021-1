1fd2ca33228932ccf030c6a2b30cb5f499c5a748
Pearl Park <pearl.park@samsung.com>
2021-05-17 22:16:49 +0900

    add scenemanager

---
+++ MainProgram/scenemanager.py
@@ -0,0 +1,10 @@
+#import datamanager as dm
+#import graphmanager as gm
+#import analysismanager as am
+#import mapmanager as mm
+
+#def exampleFunction():
+    #return something
+
+if name == 'main':
+    #call function
