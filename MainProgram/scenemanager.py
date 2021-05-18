591c3c929d0a6206d8d0a336e50ec70ae6b68476
Pearl Park <pearl.park@samsung.com>
2021-05-19 02:22:47 +0900

    Abstract SceneManager

--- MainProgram/scenemanager.py
+++ MainProgram/scenemanager.py
@@ -3,8 +3,47 @@
 #import analysismanager as am
 #import mapmanager as mm
 
-#def exampleFunction():
+#input = 0 #user input code (temp)
+#categories = []
+#return_data #pandas.DataFrame
+#return_graph #format undecided (expect plot)
+#return_map #format undecided
+
+#def ShowMainScreen():
+    #nonValue-Returning
+    
+#def SelectMenu():
     #return something
+
+#def OrSelectCategory():
+    #return something
+
+#def AndSelectCategory():
+    #return something
+
 
 if name == 'main':
     #call function
+    #iteration for program
+
+    #ShowMainScreen()
+    #input = SelectMenu()
+
+    #Process by case depending on the input
+    #if(input == )
+
+    #Data,Graph,Analysis
+    #categories = OrSelectCategory()
+    #return_data = dm.DataSearch(categories)
+    #return_data = am.DataSearch(categories)
+    #return_graph = gm.GetGraph(categories)
+
+    #Map
+    #categories = AndSelectCategory()
+    #return_map = mm.GetMap(categories)
+
+    #Display returns
+
+    #End Process
+
+    
