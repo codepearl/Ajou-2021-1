8e958a28efe42f167e480aeb89afe0584c365d78
Pearl Park <pearl.park@samsung.com>
2021-05-20 01:59:24 +0900

    code review by pearl

--- MainProgram/datamanager.py
+++ MainProgram/datamanager.py
@@ -10,5 +10,33 @@ def DataSearch(isArea, li, manager_name):
     return data
 
 
-# data = DataSearch(1, 2, 3)
-# print(data)
+if __name__ == '__main__':
+    data = DataSearch(1, 2, 3)
+    print(data)
+
+'''
+CODE REIVEW by pearl
+
+1. csvLoad 필요
+
+2. csv에서 Load한 data를 담을 변수 선언 필요(전역 변수)
+
+3. manager_name에 따라 다르게 처리하는 과정 필요
+
+4. 특정 열만 load하거나, 특정 열만 return 하는 열 처리 과정 필요
+
+5. isArea, li에 따른 data 추리는 과정 필요
+
+6. if __name__ == '__main__':
+해당줄을 넣으면, graphmanager를 직접적으로 실행했을때에만 해당 부분이 실행됩니다.
+다른 곳에서 import를 하거나 실행시켜도 main문은 직접 datamanager를 실행한게 아니라면
+실행되지 않습니다. 그러므로 모듈 테스트는 해당 if 문 안에서 하면 됩니다.
+
+우리 프로그램의 꽃 datamanager입니다. 매우 분량이 많아 걱정이 되는데
+분명 잘 해낼거라고 믿습니다 ㅎㅎ 잘 해낸 만큼 점수도 잘 받아갈수 있는거니까요~
+
+다른 사람들의 경우 띄어쓰기, 줄바꿈, 변수 이름, 함수 이름들을 많이 고쳐줬는데
+정말 하나도 고칠 게 없을 정도로 완벽합니다.
+
+이 주석은 다음 코딩 때 전부 지우면 됩니다.
+'''
--- MainProgram/mapmanager.py
+++ MainProgram/mapmanager.py
@@ -1,7 +1,6 @@
 import folium
 import pandas as pd
 import datamanager as dm
-import scenemanager as sm
 
 manager_name = "mapmanager"
 
@@ -24,6 +23,7 @@ manager_name = "mapmanager"
     elif isArea == 5:
         print('```') # 위도,경도, 상호명, etc..(지도에 표시할 내용)
 '''
+
 # 시군구, 업종 대분류 받고 넘겨줌 – 0 
 # 시군구, 업종 대분류/중분류 넘겨줌 – 1                    
 # 시군구, 업종 대분류/중분류/소분류 넘겨줌 – 2 
@@ -33,25 +33,46 @@ manager_name = "mapmanager"
 
 # 위에 0,1,2,3,4,5는 request_type
 
-def Requestdata(): # scenemanager Requestdata
-
-    request_type, data = sm.request()
-
-    return reqest_type, data
+def Map(request_type, li): #call from scenemanager
+    mapData = ShowLoc(request_type, li)
+    return mapData
     
     
-def Getloc(): # Datamanager return
+def GetLoc(request_type, li): # Datamanager return
+    return dm.DataSearch(request_type, li, manager_name) # 해당 조건에 부합하는 Data Frame return 받음 ex) 위도,경도, 상호명, etc..(지도에 표시할 내용)
 
-    request_type, li = requestdata()
+
+def ShowLoc(request_type, li): # 위도,경도, 상호명, etc..(지도에 표시할 내용) 받아서 맵에서 표시
+    data = Getloc(request_type, li)
+    #folium 사용해서 지도에 표시
+    return data
     
-    return dm.DataSearch( request_type , li , manager_name) # 해당 조건에 부합하는 Data Frame return 받음 ex) 위도,경도, 상호명, etc..(지도에 표시할 내용)
+'''
+CODE REIVEW by pearl
 
+Getloc, Showloc -> GetLoc, ShowLoc
 
-def Showloc(): # 위도,경도, 상호명, etc..(지도에 표시할 내용) 받아서 맵에서 표시
+1. request_type 에 대한 변수 선언 필요
+manager_name 다다음 라인에 임시 int 넣어서 변수 선언하세요.
 
-    data = Getloc()
+2. 아직 각 모듈 간 과정에 대한 이해가 조금 부족한 것 같습니다.
+sm에서 mm의 함수를 호출하는 형식이므로, mm에서는 sm을 호출할 필요는 없습니다.
+->import scenemanager 삭제
+그러므로 Requestdata 아닌, sm에서 호출될 이름으로 변경하는게 좋습니다.
+Requestdata() -> Map(request_type, li)
 
-    
+sm에서 Map을 호출해 request_type과 string list들을 넘겨줄 예정입니다.
 
+ShowLoc을 호출하여 결과값을 mapData에 담아 sm에 돌려줄 것입니다.
 
+일단 최대한 구조를 냅두면서 각 함수를 조금 수정해봤습니다.
 
+GetLoc/ShowLoc에 각각 파라미터를 추가함으로써,
+sm에서 호출될 때 받아온 값들을 dm에 넘겨 데이터들을 받고 -> GetLoc의 내용
+folium 사용해서 지도에 표시합니다. ->ShowLoc의 내용
+
+각 함수 정의가 매우 마음에 듭니다.
+mapmanager 이 모듈 하나에 대한 이해는 매우 좋아 기대가 됩니다.
+
+이 주석은 다음 코딩 때 전부 지우면 됩니다.
+'''
