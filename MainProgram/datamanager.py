caebe16dd229207ee5c7bfe2d05f495993fed9c6
249-IN-THE-BUIL\ssagu <newgrain3@gmail.com>
2021-06-03 19:15:28 +0900

    modified datamanager to fix the problems from am, mm / modifying graphmanager to fix problems about texts

--- MainProgram/datamanager.py
+++ MainProgram/datamanager.py
@@ -4,18 +4,25 @@ pure_data = pd.read_csv('Dummy_File.csv', encoding='cp949')
 
 
 def DataSearch(isArea, li, manager_name):
-
-    if isArea:
-        # 입력으로 지역이 될 경우, 현재 2개 모두 입력되어야 함
+    if manager_name != 'mapmanager':
+        if isArea:
+            # 입력으로 지역이 될 경우, 현재 2개 모두 입력되어야 함
+            m_district = pure_data['시군구명'] == li[0]
+            s_district = pure_data['법정동명'] == li[1]
+            input_based_data = pure_data[m_district & s_district]
+        else:
+            # 입력으로 업종이 입력될 경우, 현재 3개 모두 입력되어야 함
+            l_class = pure_data['상권업종대분류명'] == li[0]
+            m_class = pure_data['상권업종중분류명'] == li[1]
+            s_class = pure_data['상권업종소분류명'] == li[2]
+            input_based_data = pure_data[l_class & m_class & s_class]
+    else:
         m_district = pure_data['시군구명'] == li[0]
         s_district = pure_data['법정동명'] == li[1]
-        input_based_data = pure_data[m_district & s_district]
-    else:
-        # 입력으로 업종이 입력될 경우, 현재 3개 모두 입력되어야 함
-        l_class = pure_data['상권업종대분류명'] == li[0]
-        m_class = pure_data['상권업종중분류명'] == li[1]
-        s_class = pure_data['상권업종소분류명'] == li[2]
-        input_based_data = pure_data[l_class & m_class & s_class]
+        l_class = pure_data['상권업종대분류명'] == li[2]
+        m_class = pure_data['상권업종중분류명'] == li[3]
+        s_class = pure_data['상권업종소분류명'] == li[4]
+        input_based_data = pure_data[m_district & s_district & l_class & m_class & s_class]
 
     if manager_name == 'mapmanager':
         handled_data = MMSearch(input_based_data)
@@ -36,20 +43,20 @@ def DataSearch(isArea, li, manager_name):
 
 
 def MMSearch(input_based_data):
-    mm_data = pd.DataFrame(input_based_data, columns=['상호명','경도','위도'])
+    mm_data = pd.DataFrame(input_based_data, columns=['상권업종대분류명', '상호명','경도','위도'])
     return mm_data
 
 
 def GMSearch(input_based_data):
-    gm_data = pd.DataFrame(input_based_data, columns=['상호명','지점명','상권업종대분류명','상권업종중분류명','상권업종소분류명','시군구명','법정동명'])
+    gm_data = pd.DataFrame(input_based_data, columns=['상권업종대분류명','상권업종중분류명','상권업종소분류명','시군구명','법정동명'])
     return gm_data
 
 
 def AMSearch(input_based_data, isArea):
     if isArea:
-        am_data = pd.DataFrame(input_based_data, columns=['상권업종대분류명','상권업종중분류명','상권업종소분류명'])
+        am_data = pd.DataFrame(input_based_data, columns=['상권업종소분류명'])
     else:
-        am_data = pd.DataFrame(input_based_data, columns=['시군구명','법정동명'])
+        am_data = pd.DataFrame(input_based_data, columns=['법정동명'])
     return am_data
 
 
@@ -71,7 +78,6 @@ def ListSDistrict(m_district):
 
     # 업종분류
 
-
 def ListLCategory():
     return pure_data['상권업종대분류명'].unique()
 
@@ -87,11 +93,11 @@ def ListSCategory(m_district):
 
 
 if __name__ == '__main__':
-    # temp_list = ['세종특별자치시', '조치원읍']
-    # data = DataSearch(True, temp_list,'datamanager')
+    temp_list = ['세종특별자치시', '조치원읍', '음식', '유흥주점', '호프/맥주']
+    data = DataSearch(True, temp_list,'mapmanager')
     # data = ListSDistrict('세종특별자치시')
     # data = ListMDistrict()
-    data = ListSCategory('개인서비스')
+    # data = ListSCategory('개인서비스')
     print(data)
 
 '''
--- MainProgram/graphmanager.py
+++ MainProgram/graphmanager.py
@@ -1,20 +1,26 @@
 import datamanager as dm
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
 
 manager_name = "graphmanager"
 
 
-def GetGraph(isArea, li, manager_name):
+def GetCountplot(isArea, li, manager_name):
     pure_data = dm.DataSearch(isArea, li, manager_name)
     plt.rc('font', family='NanumGothic')
-    return_countplot = MakecountplotGraph(pure_data, isArea)
-    return_pie = MakePieGraph(pure_data, isArea)
-    return return_countplot, return_pie
-    # return return_countplot_graph
+    return_countplot = Makecountplot(pure_data, isArea)
+    return return_countplot
 
 
-def MakecountplotGraph(pure_data, isArea):
+def GetPie(isArea, li, manager_name):
+    pure_data = dm.DataSearch(isArea, li, manager_name)
+    plt.rc('font', family='NanumGothic')
+    return_pie = MakePie(pure_data, isArea)
+    return return_pie
+
+
+def Makecountplot(pure_data, isArea):
     if isArea:
         made_countplot = sns.countplot(x='상권업종대분류명', data=pure_data)
     else:
@@ -23,10 +29,54 @@ def MakecountplotGraph(pure_data, isArea):
     return made_countplot
 
 
-def MakePieGraph(pure_data, isArea):
+def MakePie(pure_data, isArea):
     # if isArea:
-    ratio_list, labels_list = MakePieData(pure_data, isArea)
-    made_pie = plt.pie(ratio_list, labels=labels_list, autopct='%.1f%%')
+    frequency, labels, explode = MakePieData(pure_data, isArea)
+
+    fig = plt.figure(figsize=(8,8))
+    fig.set_facecolor('white')
+    ax = fig.add_subplot()
+
+    made_pie = plt.pie(frequency, labels=labels, autopct='%.1f%%', startangle=90, counterclock=False, explode=explode)
+
+    #글자 겹침 해결
+    total = np.sum(frequency)
+    threshold = 5
+    sum_pct = 0
+
+    bbox_props = dict(boxstyle='square', fc='w', ec='w', alpha=0)  ## annotation 박스 스타일
+
+    ## annotation 설정
+    config = dict(arrowprops=dict(arrowstyle='-'), bbox=bbox_props, va='center')
+
+    for i, l in enumerate(labels):
+        ang1, ang2 = ax.patches[i].theta1, ax.patches[i].theta2  ## 파이의 시작 각도와 끝 각도
+        # center, r = ax.patches[i].center, ax.patches[i].r  ## 원의 중심 좌표와 반지름길이
+
+        if i < len(labels) - 1:
+            sum_pct += float(f'{frequency[i] / total * 100:.2f}')
+            text = f'{frequency[i] / total * 100:.2f}%'
+        else:  ## 마지막 파이 조각은 퍼센티지의 합이 100이 되도록 비율을 조절
+            text = f'{100 - sum_pct:.2f}%'
+
+        ## 비율 상한선보다 작은 것들은 Annotation으로 만든다.
+        # if frequency[i] / total * 100 < threshold:
+        ang = (ang1 + ang2) / 2  ## 중심각
+        x = np.cos(np.deg2rad(ang))  ## Annotation의 끝점에 해당하는 x좌표
+        y = np.sin(np.deg2rad(ang))  ## Annotation의 끝점에 해당하는 y좌표
+
+        ## x좌표가 양수이면 즉 y축을 중심으로 오른쪽에 있으면 왼쪽 정렬
+        ## x좌표가 음수이면 즉 y축을 중심으로 왼쪽에 있으면 오른쪽 정렬
+        horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+        connectionstyle = "angle,angleA=0,angleB={}".format(ang)  ## 시작점과 끝점 연결 스타일
+        config["arrowprops"].update({"connectionstyle": connectionstyle})  ##
+        ax.annotate(text, xy=(x, y), xytext=(1.5 * x, 1.2 * y), horizontalalignment=horizontalalignment, **config)
+        # else:
+        #     x = (r / 2) * np.cos(np.pi / 180 * ((ang1 + ang2) / 2)) + center[0]  ## 텍스트 x좌표
+        #     y = (r / 2) * np.sin(np.pi / 180 * ((ang1 + ang2) / 2)) + center[1]  ## 텍스트 y좌표
+        #     ax.text(x, y, text, ha='center', va='center', fontsize=12)
+    plt.legend(made_pie[0], labels, loc='upper right')
+
     # else:
     #     ratio_list, labels_list = MakePieData(pure_data, isArea)
     #     made_pie = plt.pie(ratio_list, labels=labels_list, autopct='%.1f%%')
@@ -40,26 +90,30 @@ def MakePieData(pure_data, isArea):
         list_filtered_data2 = list(set_filtered_data)
         count_list = []
         ratio_list = []
+        explode = []
         for i in list_filtered_data2:
             count_list.append((list_filtered_data.count(i)))
         for j in count_list:
             ratio_list.append(j / len(list_filtered_data) * 100)
+            explode.append(0.1)
     else:
         list_filtered_data = pure_data['법정동명'].tolist()
         set_filtered_data = set(list_filtered_data)
         list_filtered_data2 = list(set_filtered_data)
         count_list = []
         ratio_list = []
+        explode = []
         for i in list_filtered_data2:
             count_list.append((list_filtered_data.count(i)))
         for j in count_list:
             ratio_list.append(j / len(list_filtered_data) * 100)
-    return ratio_list, list_filtered_data2
+            explode.append(0.1)
+    return ratio_list, list_filtered_data2, explode
 
 
 if __name__ == '__main__':
-    test_return_graph1 = GetGraph(True, ['세종특별자치시', '조치원읍'], "graphmanager")
-    # test_return_graph1 = GetGraph(False, ['음식', '한식', '갈비/삼겹살'], "graphmanager")
+    # return_graph = GetCountplot(False, ['음식', '한식', '갈비/삼겹살'], "graphmanager")
+    return_graph = GetPie(False, ['음식', '한식', '갈비/삼겹살'], "graphmanager")
     plt.show()
 
 '''
